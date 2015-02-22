b5fe0f20f4ecc5789a53a0fd7a16562eb0851c28
Nauman Ahmad <nauman-ahmad@outlook.com>
2015-02-22 11:20:58 +0500

    Info Window details added

--- app.js
+++ app.js
@@ -54,12 +54,20 @@ if (Meteor.isClient) {
       var infowindow = new google.maps.InfoWindow();
 
       Events.find().forEach(function (doc) {
-        var name = "<h1>"+doc.name+"</h1>";
+        var name = "<h4>"+ doc.name + "</h4>";
+        var date = "<p><b>Date:</b> " + doc.date + "</p>";
+        var time = "<p><b>Time:</b> " + doc.time + "</p>";
+        var host = "<p><b>Host:</b> " + doc.username + "</p>";
+        var des = "<p>" + doc.description + "</p>";
+        var details = "<p>" + doc.requirements + "</p>";
+
+        var content = name + date + time + host + des + details;
+
         var marker = new google.maps.Marker({
           position: new google.maps.LatLng(doc.lat, doc.lng),
           animation: google.maps.Animation.BOUNCE,
           map: map.instance,
-          title: name
+          title: content
         });
 
         google.maps.event.addListener(marker, 'click', function() {
