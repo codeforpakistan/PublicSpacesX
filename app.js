f4517c15d41041d181a6befcb166e982bc2ca3c0
Thierry Delbart <thierry@thde.pro>
2016-02-11 12:38:29 -0200

    Loading places from pref datas

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 Events = new Mongo.Collection('Events');
+Places = new Mongo.Collection('Places');
 
 var gLati = -25.441105;
 var gLongi =-49.276855;
@@ -99,7 +100,6 @@ if (Meteor.isClient) {
 
         var marker = new google.maps.Marker({
           position: new google.maps.LatLng(doc.lat, doc.lng),
-          animation: google.maps.Animation.BOUNCE,
           map: map.instance,
           title: content
         });
@@ -116,20 +116,22 @@ if (Meteor.isClient) {
   Template.places.created = function(){
     GoogleMaps.ready('places', function(map) {
       var infowindow = new google.maps.InfoWindow();
-/*
-      Events.find().forEach(function (doc) {
-        var name = "<h4>"+ doc.name + "</h4>";
-        var date = "<p><b>Date:</b> " + doc.date + "</p>";
-        var time = "<p><b>Time:</b> " + doc.time + "</p>";
-        var host = "<p><b>Host:</b> " + doc.username + "</p>";
-        var des = "<p>" + doc.description + "</p>";
-        var details = "<p>" + doc.requirements + "</p>";
 
-        var content = name + date + time + host + des + details;
+      Places.find().forEach(function (doc) {
+        var content = 
+        	"<h4>"+ doc.NM_ABREV_EQUI + "</h4>" +
+        	"<p><strong>"+ doc.DS_TEMA + "</strong><p>" +
+        	"<p><em>"+ doc.DS_SUBTIPO_EQUIPAMENTO + "</em><p>";
+        	
+        if (doc.TELEFONE_EQUI) {
+        	content = content + "<p><b>Fone:</b> " + doc.TELEFONE_EQUI + "</p>"
+        }
+
+    	lat = parseInt(doc.LAT_SIRGAS.replace(/\s/g, '').substr(0,9), 10) / 1000000;
+    	lon = parseInt(doc.LON_SIRGAS.replace(/\s/g, '').substr(0,9), 10) / 1000000;
 
         var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(doc.lat, doc.lng),
-          animation: google.maps.Animation.BOUNCE,
+          position: new google.maps.LatLng(lat, lon),
           map: map.instance,
           title: content
         });
@@ -139,7 +141,7 @@ if (Meteor.isClient) {
             infowindow.open(map.instance, this);
         });
 
-      });*/
+      });
     });
   }
 
@@ -219,27 +221,66 @@ if (Meteor.isServer) {
 
 	Meteor.methods({
 	
-	    reload_places: function() {
+
+	    reset_places: function() {
+
+			console.log(">---------- Starting resetting places");
 	    	
-//			Papa.parse(process.env.ROOT_URL + "data/Unidades_Atendimento_Ativas_Curitiba_-_Base_de_Dados.csv", {
-/*myobject = HTTP.get(Meteor.absoluteUrl("data/Unidades_Atendimento_Ativas_Curitiba_-_Base_de_Dados.csv"));
-console.log(myobject);
-console.log(Meteor.absoluteUrl("data/Unidades_Atendimento_Ativas_Curitiba_-_Base_de_Dados.csv"));
-*/
-console.log(Meteor.absoluteUrl("data/unidades.csv"));
-			Papa.parse("http://wannago/unidades.csv", {
+	    	console.log('Places to delete: ' + Places.find().fetch().length);
+	    	Places.remove({});
+	    	console.log('Places removed');
+	    	
+	    	Meteor.call('reload_places');
+	    	
+			console.log(">---------- Finished resetting places");
+	    },
+
+
+	    reload_places: function() {
+
+			console.log(">---------- Starting reloading places");
+
+			Papa.parse(Assets.getText("data/unidades.csv"), {
 				download: true,
 				header: true,
 				skipEmptyLines: true,
 				dynamicTyping: true,
-				preview: 5,
-				worker: true,
+//				preview: 100,
+				encoding: "UTF8",
 				step: function(results, parser) {
-					console.log("Row data:", results.data);
-					console.log("Row errors:", results.errors);
+					if (results.errors && results.errors.length) {
+						console.log("Parsing row errors:", results.errors);
+					}
+					
+					if (results.data[0]) {
+						
+						data = results.data[0];
+						data.imported_at = Date.now();
+						
+						if (data.DS_DEP_ADMINISTRATIVA != 'Particular') {
+							
+							console.log("Loading: ".data.NM_ABREV_EQUI);
+							
+							Places.upsert({
+							    // Selector
+							    source: "CD_EQUI",
+							    currency: data.CD_EQUI
+							}, {
+							    // Modifier
+							    $set: data
+							});
+						}
+						
+						
+					}
+					
 				},
-  				complete: function(results) {
-					console.log("Finished:", results);
+				complete: function(results) {
+					if (results.errors && results.errors.length) {
+						console.log("Parsing errors:", results.errors);
+					}
+					
+					console.log(">---------- Finished reloading places");
 				}
 			});
 			
